8e1ee65d818381355210f88e06d91a33a9657655
Your Name <Nimish>
2016-10-27 17:27:16 +0530

    Edited

--- factorial.java
+++ factorial.java
@@ -1,17 +1,15 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
-class Factorial
-    {  
-     public static void main(String args[])
-      {  
-        int i,fact=1;  
-        int number=3;
-        for(i=1;i<=number;i++
-        {    
-          fact=fact*i;    
-        }    
-      System.out.println("Factorial is: "+fact);    
-      }  
-    }  
+class Factoral
+{
+	public static void main(String args[])
+	{
+		int i, j, fact=1, n;
+		System.out.println("Enter the number: ");
+		int n = Integer.parseInt(System.console().readLine());//Input the number
+		for(i=1;i<=n;i++)
+		{ fact = fact * i;}
+		System.out.println("Factorial of "+n+" is "+f);
+	
+	}
+}
