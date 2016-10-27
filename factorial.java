732232917ff3b61dbb65bd075ef75d0cf44272e7
Your Name <Nimish>
2016-10-27 17:46:08 +0530

    Staging test

--- factorial.java
+++ factorial.java
@@ -4,7 +4,7 @@ class Factoral
 {
 	public static void main(String args[])
 	{
-		int i, j, fact=1, n;
+		int i, j, fact=1, n;//staging
 		System.out.println("Enter the number: ");
 		int n = Integer.parseInt(System.console().readLine());//Input the number from user
 		for(i=1;i<=n;i++)
