a1bdcf2d30d3d7ed5c033271041ebba2162c3c32
experionnimish <nimish.mathew@experionglobal.com>
2016-10-27 17:31:32 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -6,7 +6,7 @@ class Factoral
 	{
 		int i, j, fact=1, n;
 		System.out.println("Enter the number: ");
-		int n = Integer.parseInt(System.console().readLine());//Input the number from user
+		int n = Integer.parseInt(System.console().readLine());//Input the number from user ok ok ok
 		for(i=1;i<=n;i++)
 		{ fact = fact * i;}
 		System.out.println("Factorial of "+n+" is "+f);
