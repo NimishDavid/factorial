e293f91dd764dfe28fdd76e6d421d1761c8fb0b7
Your Name <Nimish>
2016-10-27 13:04:11 +0530

    First Commit

---
+++ factorial.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Factorial
+    {  
+     public static void main(String args[])
+      {  
+        int i,fact=1;  
+        int number=3;
+        for(i=1;i<=number;i++
+        {    
+          fact=fact*i;    
+        }    
+      System.out.println("Factorial is: "+fact);    
+      }  
+    }  
